4bc365edd98ccafeca965ee121c95e127fe7cd6d
T-Andrei-C <115529065+T-Andrei-C@users.noreply.github.com>
2022-10-31 20:55:24 +0200

    Update algorithm.js

--- algorithm.js
+++ algorithm.js
@@ -1,1 +1,5 @@
-
+let favoriteBooks = [{title: ("The Lord of the Rings"), author: "J.R.R Tolkein", year: 1954, isNewerThan2000: false, age: 2022 - 1954, characters: ["Aragorn", "Frodo", "Gandalf", "Sam"]},
+                     {title: ("Harry Potter"), author: "J.K Rowling", year: 1997, isNewerThan2000: false, age: 2022 - 1997, characters:["Borgin", "Perkins", "Trevor", "Winky"]}];
+for (let title of favoriteBooks){
+    console.log(title);
+}
