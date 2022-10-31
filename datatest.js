7246be6b8ea67a61fcfd5c277b08f2b51e2c7dd1
T-Andrei-C <115529065+T-Andrei-C@users.noreply.github.com>
2022-10-31 17:48:34 +0200

    Better solution for data basics

--- datatest.js
+++ datatest.js
@@ -1,1 +1,6 @@
 
+let favoriteBooks = [{title: "The Lord of the Rings", author: "J.R.R Tolkein", year: 1954, isNewerThan2000: false, age: 2022 - 1954, characters: ["Aragorn", "Frodo", "Gandalf", "Sam"]},
+                    {title: "Harry Potter", author: "J.K Rowling", year: 1997, isNewerThan2000: false, age: 2022 - 1997, characters:["Borgin", "Perkins", "Trevor", "Winky"]}];
+console.log(favoriteBooks);
+console.log(favoriteBooks[1].title + " " + favoriteBooks[1].characters[0]);
+console.log(favoriteBooks[0].age - favoriteBooks[1].age);
